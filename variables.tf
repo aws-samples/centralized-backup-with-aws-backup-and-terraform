37bd238c182ef05870e794c418e99ad6cdc00579
amianjar <amianjar@amazon.com>
2024-02-15 21:54:03 +0530

    Validation logic added for AWS Backup account id

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "backup_account_id" {
   type        = string
   default     = ""
   validation {
-    condition = can(regex("^\\d{12}$", var.target_account_id))
+    condition = can(regex("^\\d{12}$", var.backup_account_id))
     error_message = "String must be a 12-digit number."
   }
 }
